f9e8ea5010e98ce34aacfa2db7b6698f6bac4c7e
gorgor15 <56014950+gorgor15@users.noreply.github.com>
2021-10-05 00:34:07 +0900

    Add files via upload

---
+++ 5주차/LRUD.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int n;
+int x=1,y=1;
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+char moveTypes[4] = {'L', 'R', 'U', 'D'};
+char str[100], plans[100];
+
+
+int main(void){
+    scanf("%d", &n);
+    int count = 0, cnt = 0;
+    scanf("%[^n]s", str);
+
+
+    while(1){
+        count++;
+        if(str[count]=='\n' || '\0') break;
+        if(str[count]== ' ') continue;
+
+        plans[cnt]=str[count];
+        char plan = plans[cnt];
+      
+        int nx = -1, ny = -1;
+        for(int j=0; j<4; j++){
+            if(plan == moveTypes[j]){
+                nx = x + dx[j];
+                ny = y + dy[j];
+            }
+        }
+        if(nx < 1 || ny < 1 || nx > n || ny > n) continue;
+       
+        x = nx;
+        y = ny;
+        cnt++;
+    }
+    printf("%d %d \n", x ,y);
+    return 0;
+
+}
---
+++ 5주차/night.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void) {
+    char input[1];
+    
+    scanf("%s", &input);
+    
+    int row = input[1] - '0';
+    int column = input[0] - 'a' + 1;
+   
+    int dx[] = {-2, -1, 1, 2, 2, 1, -1, -2};
+    int dy[] = {-1, -2, -2, -1, 1, 2, 2, 1};
+   
+    int result = 0;
+    for (int i = 0; i < 8; i++) {
+        int nextRow = row + dx[i];
+        int nextColumn = column + dy[i];
+        if (nextRow >= 1 && nextRow <= 8 && nextColumn >= 1 && nextColumn <= 8) {
+            result += 1;
+        }
+    }
+    printf("%d",result);
+    return 0;
+}
